8a66d920051830db16b2fa97c4896d263336076d
MAURICIO MANUEL F. BERGANCIA <mauriciomanuel.bergancia@wvsu.edu.ph>
2023-09-09 07:46:51 +0800

    Add files via upload
    first task

---
+++ Bergancia/src/BerganciaMauricioManuel.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class BerganciaMauricioManuel {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("First Name: ");
+        String Fname = sc.nextLine();
+
+        System.out.print("Last Name: ");
+        String Lname = sc.nextLine();
+
+        System.out.print("Middle Initial: ");
+        String MiddleI = sc.next();
+
+        System.out.print("ID Number: ");
+        String Id = sc.next();
+
+        System.out.print("Email Address: ");
+        String Email = sc.next();
+
+
+        System.out.printf("hello %s %s %s\n", Fname,MiddleI, Lname);
+        System.out.println("Your ID Number is: " + Id);
+        System.out.println("Your Email is: " + Email);
+    }
+}
